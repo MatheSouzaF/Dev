22ea73129a357845f6864d432786114cf9ee76a9
Matheus de souza <matheusouzafloriani@gmail.com>
2022-09-05 17:07:14 -0300

    MasterClass Rocketseat

---
+++ MasterClass/variaveis/variaveis.js
@@ -0,0 +1,21 @@
+// if(true){
+//     var pi = 3.141592
+// }
+// console.log(pi);
+
+// if (true){
+
+//     let pi = 3.141592 
+// }
+// console.log(pi)
+
+// if (true){
+
+//     const pi = 3.141592
+// }
+// console.log(pi)
+
+// (function () {
+//     var pe = 3.141592
+// })()
+// console.log(pi)
