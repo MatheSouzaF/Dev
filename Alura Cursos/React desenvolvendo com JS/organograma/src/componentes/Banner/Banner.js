f858c9dc0949a86ce54f8fce58c0c3c48057193a
Matheus de souza <matheusouzafloriani@gmail.com>
2022-09-05 22:38:17 -0300

    começo do curso de React

---
+++ Alura Cursos/React desenvolvendo com JS/organograma/src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+    import './Banner.css'
+
+function Banner() {
+
+    //JSX
+    return (
+        <header className='banner'>
+            <img src="/img/banner.png" alt="O banner da página do Organo" />
+        </header>
+    )
+}
+
+export default Banner
