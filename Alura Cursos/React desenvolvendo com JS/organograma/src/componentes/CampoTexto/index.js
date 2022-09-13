ac99d5e91ba585a6c02193f246ddad4b33c322b8
Matheus de souza <matheusouzafloriani@gmail.com>
2022-09-12 23:24:49 -0300

    useStates

--- Alura Cursos/React desenvolvendo com JS/organograma/src/componentes/CampoTexto/index.js
+++ Alura Cursos/React desenvolvendo com JS/organograma/src/componentes/CampoTexto/index.js
@@ -1,12 +1,23 @@
-import './CampoTexto.css'
+import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
-    return (
-        <div className="campo-texto">
-            <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}></input>
-        </div>
-    )
-}
+  const placeholderModificada = `${props.placeholder}...`;
 
-export default CampoTexto
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value);
+  };
+
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input
+        value={props.valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={placeholderModificada}
+      />
+    </div>
+  );
+};
+
+export default CampoTexto;
