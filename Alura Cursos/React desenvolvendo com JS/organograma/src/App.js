9448ef1f6af025746bde5b55ef6cdbfbc7654af4
Matheus de souza <matheusouzafloriani@gmail.com>
2022-09-19 16:07:42 -0300

    Começando a fazer o componente de Time

--- Alura Cursos/React desenvolvendo com JS/organograma/src/App.js
+++ Alura Cursos/React desenvolvendo com JS/organograma/src/App.js
@@ -1,20 +1,28 @@
-import { useState } from 'react';
-import Banner from './componentes/Banner';
-import Formulario from './componentes/Formulario';
+import { useState } from "react";
+import Banner from "./componentes/Banner";
+import Formulario from "./componentes/Formulario";
+import Time from "./componentes/Time";
 
 function App() {
-
-  const [colaboradores, setColaboradores] = useState([])
+  const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
-    setColaboradores([...colaboradores, colaborador])
-  }
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+  };
 
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorAdicionado(colaborador)
+        }
+      />
+      <Time nome="Programação" />
+      <Time nome="Front-end" />
+      <Time nome="Data Science" />
+      <Time nome="Devops" />
     </div>
   );
 }
