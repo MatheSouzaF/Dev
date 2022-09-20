c714f9454cf7d47ccbf6266f61fb60e696752ca8
Matheus de souza <matheusouzafloriani@gmail.com>
2022-09-20 11:02:17 -0300

    Add Time, filtrar Time, criação do Rodapé

--- Alura Cursos/React desenvolvendo com JS/organograma/src/App.js
+++ Alura Cursos/React desenvolvendo com JS/organograma/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
+import Rodape from "./componentes/Rodape";
 import Time from "./componentes/Time";
 
 function App() {
@@ -57,8 +58,15 @@ function App() {
         }
       />
       {times.map((time) => (
-        <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />
+        <Time
+         key={time.nome}
+         nome={time.nome} 
+         corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria} 
+          colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+          />
       ))}
+      <Rodape/>
     </div>
   );
 }
