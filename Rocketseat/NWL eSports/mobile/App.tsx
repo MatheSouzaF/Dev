6f004b2493a5b2dbb8e9f9f19a03d8f9657a89ab
Matheus de souza <matheusouzafloriani@gmail.com>
2022-09-21 16:36:45 -0300

    Ep 1 completo NLW eSports

---
+++ Rocketseat/NWL eSports/mobile/App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Oi Matheus tudo bem</Text>
+      <Button title="Send 1" />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+interface ButtonProps{
+  title: string;
+}
+
+function Button(props: ButtonProps){
+  return(
+    <TouchableOpacity>
+      <Text>
+    {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f22341',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- Rocketseat/NWL eSports/server/src/server.ts
+++ Rocketseat/NWL eSports/server/src/server.ts
@@ -4,8 +4,10 @@ const app = express();
 
 app.get("/ads", (request, response) => {
   return response.json([
-    {id: 1, name: 'Anúncio 1'}
-    {id: 2, name: 'Anúncio 2'}
+    {id: 1, name: 'Anúncio 1'},
+    {id: 2, name: 'Anúncio 2'},
+    {id: 3, name: 'Anúncio 3'},
+    {id: 3, name: 'Anúncio 3'},
     {id: 3, name: 'Anúncio 3'}
   ]);
 });
