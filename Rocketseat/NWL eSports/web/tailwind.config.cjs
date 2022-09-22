1b62447fbca8633ca4e6830ef21713865e8607ca
Matheus de souza <matheusouzafloriani@gmail.com>
2022-09-22 17:29:33 -0300

    Importando phosphor e criando Button

--- Rocketseat/NWL eSports/web/tailwind.config.cjs
+++ Rocketseat/NWL eSports/web/tailwind.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
       backgroundImage: {
         galaxy: "url('/backgroundGalaxy.png')",
         "nlw-gradient":"linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 74.57%, #E1D55D 3.94% )",
+        'game-gradient':  'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       },
     },
   },
