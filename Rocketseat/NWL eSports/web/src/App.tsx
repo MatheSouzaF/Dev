b944fdf9e9e6d5ee85421af25ec06c4e654af82d
Matheus de souza <matheusouzafloriani@gmail.com>
2022-09-23 14:14:56 -0300

    ajuste css

--- Rocketseat/NWL eSports/web/src/App.tsx
+++ Rocketseat/NWL eSports/web/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} />
 
-      <h1 className="text-6xl text-white font-black mt-20">
+      <h1 className="text-6xl text-red font-black mt-20">
         Seu{" "}
         <span className="text-transparent bg-nlw-gradient bg-clip-text">
           duo
@@ -69,14 +69,14 @@ function App() {
       <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
         <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
           <div>
-            <strong className="text-2xl text-white font-black block">
+            <strong className="text-2xl text-red font-black block">
               Não encontrou seu duo?
             </strong>
             <span className="text-zinc-400 block">
               Publique um anúncio para encontrar novos players!
             </span>
           </div>
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-red rounded flex items-center gap-3">
             <MagnifyingGlassPlus size={24}/>
             Publicar anúncio
           </button>
