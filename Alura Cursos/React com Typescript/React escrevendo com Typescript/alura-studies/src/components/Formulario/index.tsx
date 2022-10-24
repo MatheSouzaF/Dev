1d3ff5b6929d9f512a19bd8fc59f7aef5282cbc3
Matheus de souza <matheusouzafloriani@gmail.com>
2022-10-24 15:59:05 -0300

    Componentização

---
+++ Alura Cursos/React com Typescript/React escrevendo com Typescript/alura-studies/src/components/Formulario/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Button from "../Buttons";
+
+class Formulario extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <form>
+        <div>
+          <label htmlFor="tarefa">Adicionae um novo estudo</label>
+          <input
+            type="text"
+            name="tarefa"
+            id="tarefa"
+            placeholder="O que você quer estudar"
+            required
+          />
+        </div>
+        <label htmlFor="tempo">Tempo</label>
+        <input
+          type="time"
+          step="1"
+          name="tempo"
+          id="tempo"
+          min="00:00:00"
+          max="01:30:00"
+          required
+        />
+        <div></div>
+        <Button />
+      </form>
+    );
+  }
+}
+export default Formulario
