d19f0f83888078177ccf0da74cfbb7460b9e3582
Matheus de souza <matheusouzafloriani@gmail.com>
2022-11-03 11:49:06 -0300

    Printando objetos

---
+++ Alura Cursos/Back-end com JS/Objetos/print-infos.js
@@ -0,0 +1,8 @@
+const cliente = { 
+    nome:"Matheus",
+    idade: 25,
+    cpf: "12345678989",
+    email: "matheus@gmail.com"
+}
+console.log(`Meu nome é ${cliente.nome} e tenho ${cliente.idade} anos.`)
+console.log(cliente.cpf.substring(0,3))
